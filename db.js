c9a221030b549d433cd162f2094defb16d6ad698
luis.saiz1 <luis.saiz@uao.edu.co>
2023-05-02 11:17:10 -0500

    Remove info db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import {createPool} from 'mysql2/promise'
 
 export const pool = createPool({
-    host: '192.168.1.9',
-    user: 'root',
-    password: 'unico123',
-    port: '3306',
-    database: 'caol'
+    host: '',
+    user: '',
+    password: '',
+    port: '',
+    database: ''
 })
