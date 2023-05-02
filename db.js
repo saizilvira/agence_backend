2ded5348f7c4de1b0f9468acd2d88506c345d96a
saizilvira <79177201+saizilvira@users.noreply.github.com>
2023-05-02 14:35:09 -0500

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 import {createPool} from 'mysql2/promise'
 
 export const pool = createPool({
-    host: '',
-    user: '',
-    password: '',
-    port: '',
-    database: ''
+    host: 'containers-us-west-109.railway.app',
+    user: 'root',
+    password: 'DFOR7bq66VhIYdtDPJBz',
+    port: '5610',
+    database: 'railway'
 })
